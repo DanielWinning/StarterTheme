0e36ccfa1277377a8991678b5b1b087f9decbc21
Danny Winning <daniel@winningwebdesign.co.uk>
2022-02-28 17:55:46 +0000

    basic templates, npm installed tailwind

--- footer.php
+++ footer.php
@@ -1,3 +1,7 @@
-<footer>
-    Footer
-</footer>
+    </main>
+    <footer>
+        Site Footer
+    </footer>
+    <?php wp_footer(); ?>
+</body>
+</html>
--- header.php
+++ header.php
@@ -5,12 +5,19 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title><?= wp_title(); ?></title>
+    <title>
+        <?php if (!is_home()) : ?>
+            <?php single_post_title(); ?> | <?= get_bloginfo("name"); ?>
+        <?php else : ?>
+            <?php bloginfo("name") ?>
+        <?php endif; ?>
+    </title>
 
     <?php wp_head(); ?>
 
 </head>
-<body>
+<body <?php body_class(); ?>>
     <header>
         Site Header
     </header>
+    <main>
