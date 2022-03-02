6a7963385b0a831af079eab89b13b454f15361c3
Danny Winning <daniel@winningwebdesign.co.uk>
2022-03-02 23:13:49 +0000

    Tailwind working

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,38 @@
 const path = require("path");
+const {VueLoaderPlugin} = require("vue-loader");
 
 module.exports = {
-    entry: "./src/js/theme.js",
+    entry: [
+        "./src/js/theme.js",
+        "./src/scss/theme.scss"
+    ],
     output: {
         filename: "theme.js",
-        path: path.resolve(__dirname, "assets/js")
-    }
+        path: path.resolve(__dirname, "assets")
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: "vue-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            name: "[name].css",
+                            outputPath: "css/"
+                        }
+                    },
+                    "postcss-loader",
+                    "sass-loader"
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new VueLoaderPlugin()
+    ]
 }
